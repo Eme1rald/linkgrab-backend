46e833b16876fe61d4f5b8fc6d86fc6ff0ef07ae
Eme1rald <anthonyval815@gmail.com>
2025-10-28 10:32:23 +0100

    Initial commit - backend ready

---
+++ index.js
@@ -0,0 +1,14 @@
+// index.js - starter server
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("LinkGrab backend is up");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
